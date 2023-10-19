77ebc01d39809b43ba4777d72209623e3153fff3
devmasud1 <devmahmud.masu1@gmail.com>
2023-10-19 16:05:06 +0600

    apply cart delete method

--- src/Pages/MyCart/MyCart.jsx
+++ src/Pages/MyCart/MyCart.jsx
@@ -19,6 +19,21 @@ const MyCart = () => {
       text: "Product on the way....",
     });
   };
+
+  const handleCartDelete = (id) => {
+    fetch(`https://eco-tech-universe-server.vercel.app/cart/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          Swal.fire("Deleted!", "Your file has been deleted.", "success");
+          const remainingItem = cart.filter((item) => item._id !== id);
+          setCart(remainingItem);
+        }
+      });
+  };
+
   return (
     <div
       className="hero min-h-[65vh]"
@@ -26,19 +41,24 @@ const MyCart = () => {
     >
       <div className="hero-overlay bg-opacity-40"></div>
       <div className="hero-content text-center   bg-blue-100">
-        <div className="max-w-md h-60 p-8">
+        <div className="max-w-md min-h-60 p-8">
           <h1 className="mb-5 text-3xl font-bold">
             Total added item : {cart.length}
           </h1>
           {cart.map((item) => (
-            <div key={item._id}>
+            <div key={item._id} className="flex items-center gap-2 space-y-2">
               <h2>Product name: {item.name}</h2>
-              
+              <button
+                onClick={() => handleCartDelete(item._id)}
+                className="bg-red-700 px-4 text-xl font-medium text-white rounded-md"
+              >
+                X
+              </button>
             </div>
           ))}
           <button className="btn btn-neutral mt-8" onClick={handleCheckOut}>
-                checkout
-              </button>
+            checkout
+          </button>
         </div>
       </div>
     </div>
